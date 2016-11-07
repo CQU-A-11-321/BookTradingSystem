7e4f35b4dd1c9b6659e13ccf90b49a2e1dba1c94
CQU_CST_WuErli <854175782@qq.com>
2016-11-07 21:16:09 +0800

    #吴尔立  修改数据库配置

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,12 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    // 添加数据库配置信息
+    'DB_TYPE'=>'mysql',// 数据库类型
+    'DB_HOST'=>'127.0.0.1',// 服务器地址
+    'DB_NAME'=>'mydb',// 数据库名
+    'DB_USER'=>'book',// 用户名
+    'DB_PWD'=>'book',// 密码
+    'DB_PORT'=>3306,// 端口
+//    'DB_PREFIX'=>'think_',// 数据库表前缀
+    'DB_CHARSET'=>'utf8',// 数据库字符集
 );
