f7c3373821fc0523609fe6da9d4d3b1eadf6bd31
CQU_CST_WuErli <854175782@qq.com>
2016-11-09 13:49:36 +0800

    #吴尔立  增加项目目录结构示例和说明

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>321BookStore</title>
+</head>
+<body>
+welcome to 321 book store
+here is <?php echo ($result["name"]); ?>
+</body>
+</html>
