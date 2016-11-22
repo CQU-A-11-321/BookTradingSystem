f68042f47a92ee89be2b4987ff2e964ffad8f0a7
cz1996 <969176743@qq.com>
2016-11-22 18:14:33 +0800

    #程正 图书交易信息

--- Public/js/info.js
+++ Public/js/info.js
@@ -3,17 +3,35 @@
  */
 
 function display() {
-    var dis = document.getElementById("bookio");
-    dis.innerHTML="cafasds";
-    if(dis == null)
+    var dis = document.getElementById("tableone");
+    for(var i= 0;dis[i]!=null;i++)
     {
-        dis.innerHTML="cafasds";
+        var tbl = document.createElement("table");
+        tbl.style.border="1";
+        var row1 = document.createElement("tr");
+        var column1 = document.createElement("td");
+        column1.innerHTML=dis[i].name;
+        var column2 = document.createElement("td");
+        column2.innerHTML=dis[i].author;
+        var column3 = document.createElement("td");
+        column3.innerHTML=dis[i].concern;
+        var row2 = document.createElement("tr");
+        var column4 = document.createElement("td");
+        column4.innerHTML=dis[i].price;
+        var column5 = document.createElement("td");
+        column5.innerHTML=dis[i].kind;
+        var column6 = document.createElement("td");
+        column6.innerHTML=dis[i].data;
+        var column7 = document.createElement("td");
+        column7.innerHTML=dis[i].mark;
     }
-    else
-    {
-        for(var i=0;dis[i]!=null;i++)
-        {
-            document.getElementById("bookio").innerHTML=dis[i].bookid;
-        }
-    }
+}
+
+function but() {
+    var dis = document.getElementById("butto");
+    var dis = document.getElementById("butt");
+    var aa = document.createElement("a");
+    aa.href="#";
+    aa.innerHTML=dis;
+    dis.appendChild(aa);
 }
